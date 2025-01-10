0e1a76f340f0b7f7603376f46db29a8c9e614b79
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-01-10 00:05:09 -0800

    search styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [cards, setCards] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredCards, setFilteredCards] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
 
   useEffect(() => {
     fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
@@ -22,6 +22,15 @@ function App() {
     );
   }, [searchTerm, cards]);
 
+  const handleCardClick = (card) => {
+    setSelectedCards((prevSelectedCards) => [...prevSelectedCards, card]);
+    setSearchTerm(""); // Clear search term to close the dropdown
+  };
+
+  const handleDeleteLastCard = () => {
+    setSelectedCards((prevSelectedCards) => prevSelectedCards.slice(0, -1));
+  };
+
   return (
     <>
       <h1>Smallworld Bridge Generator</h1>
@@ -35,7 +44,11 @@ function App() {
         {searchTerm && (
           <div className="dropdown">
             {filteredCards.map((card) => (
-              <div key={card.id} className="dropdown-item">
+              <div
+                key={card.id}
+                className="dropdown-item"
+                onClick={() => handleCardClick(card)}
+              >
                 {card.name}
               </div>
             ))}
@@ -43,8 +56,18 @@ function App() {
         )}
       </div>
       <div className="currentHand">Hand</div>
-      <div className="deck">Deck</div>
+      <div className="deck">
+        <h2>Deck</h2>
+        {selectedCards.map((card, index) => (
+          <img
+            key={index}
+            src={card.card_images[0].image_url_small}
+            alt={card.name}
+          />
+        ))}
+      </div>
       <div className="targetMonster">Search Target</div>
+      <button onClick={handleDeleteLastCard}>Delete Previous Card</button>
     </>
   );
 }
